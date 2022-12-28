2264c1d1422b80e59687441d1d84f2d566350846
Rickyslash <r.ckyslash@gmail.com>
2022-12-28 15:45:46 +0700

    add big 3 for class Team

--- main/models/Team.java
+++ main/models/Team.java
@@ -1,4 +1,6 @@
-package src.main.models;
+package main.models;
+
+import java.util.Arrays;
 
 public class Team {
 
@@ -6,14 +8,57 @@ public class Team {
     private static final String POSITION_SEEKER = "seeker";
     private static final String POSITION_KEEPER = "keeper";
 
-/* FREQUENTLY ASKED QUESTIONS:
-    
-Question: the constants are final, so why can't we make them public? It's not possible for the caller to update them.
-  Answer: Even if the constant is final, I prefer to expose a method instead of the variable. But if you want to expose the variable, that's also correct.
+    private String house;
+    private String keeper;
+    private String seeker;
+    private String[] chasers;
 
-*/
+    public Team(String house, String keeper, String seeker, String[] chasers) {
+        this.house = house;
+        this.keeper = keeper;
+        this.seeker = seeker;
+        this.chasers = Arrays.copyOf(chasers, 3);
+    }
 
+    public Team(Team source) {
+        this.house = source.house;
+        this.keeper = source.keeper;
+        this.seeker = source.seeker;
+        this.chasers = Arrays.copyOf(source.chasers, source.chasers.length);
+    }
     
+    public String getHouse() {
+        return this.house;
+    }
+
+    public String getKeeper() {
+        return this.keeper;
+    }
+
+    public String getSeeker() {
+        return this.seeker;
+    }
+
+    public String[] getChasers() {
+        return Arrays.copyOf(this.chasers, this.chasers.length);
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+
+    public void setKeeper(String keeper) {
+        this.keeper = keeper;
+    }
+
+    public void setSeeker(String seeker) {
+        this.seeker = seeker;
+    }
+
+    public void setChasers(String[] chasers) {
+        this.chasers = Arrays.copyOf(chasers, chasers.length);
+    }
+
      public static String getPositionChaser() {
          return POSITION_CHASER;
      }
@@ -24,6 +69,14 @@ Question: the constants are final, so why can't we make them public? It's not po
 
      public static String getPositionKeeper() {
          return POSITION_KEEPER;
+     }
+
+     @Override
+     public String toString() {
+        return "House: " + this.house + "\n" +
+        "Keeper: " + this.keeper + "\n" +
+        "Seeker: " + this.seeker + "\n" +
+        "Chasers: " + Arrays.toString(this.chasers) + "\n";
      }
 
 }
